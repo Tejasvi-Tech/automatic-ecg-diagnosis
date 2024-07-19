476af4b578cc6ee87e1aab76c701605faba956b9
Tejasvi-Tech <k.tejassvi2004@gmail.com>
2024-07-19 16:27:14 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get performance on test set from hdf5')
     parser.add_argument('path_to_hdf5', type=str,
                         help='path to hdf5 file containing tracings')
-    parser.add_argument('path_to_model',  # or model_date_order.hdf5
+    parser._argument('path_to_model',  # or model_date_order.hdf5
                         help='file containing training model.')
     parser.add_argument('--dataset_name', type=str, default='tracings',
                         help='name of the hdf5 dataset containing tracings')
@@ -21,17 +21,16 @@ if __name__ == '__main__':
                         help='Batch size.')
 
     args, unk = parser.parse_known_args()
-    if unk:
-        warnings.warn("Unknown arguments:" + str(unk) + ".")
-
-    # Import data
-    seq = ECGSequence(args.path_to_hdf5, args.dataset_name, batch_size=args.bs)
+    
+ seq = ECGSequence(args.path_to_hdf5, batch_size=args.bs)
+    
     # Import model
     model = load_model(args.path_to_model, compile=False)
     model.compile(loss='binary_crossentropy', optimizer=Adam())
-    y_score = model.predict(seq,  verbose=1)
+    
+    y_score = model.predict(seq, verbose=1)
 
     # Generate dataframe
     np.save(args.output_file, y_score)
-
-    print("Output predictions saved")
+    print("Output predictions saved")   
+    
