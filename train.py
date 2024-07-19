89e473d4125408bc29ca49f6e9f9339c91bc7e4e
Tejasvi-Tech <k.tejassvi2004@gmail.com>
2024-07-19 16:57:50 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,19 +6,24 @@ import argparse
 from datasets import ECGSequence
 
 if __name__ == "__main__":
-    # Get data and train
     parser = argparse.ArgumentParser(description='Train neural network.')
-    parser.add_argument('path_to_hdf5', type=str,
-                        help='path to hdf5 file containing tracings')
-    parser.add_argument('path_to_csv', type=str,
-                        help='path to csv file containing annotations')
-    parser.add_argument('--val_split', type=float, default=0.02,
-                        help='number between 0 and 1 determining how much of'
-                             ' the data is to be used for validation. The remaining '
-                             'is used for validation. Default: 0.02')
-    parser.add_argument('--dataset_name', type=str, default='tracings',
-                        help='name of the hdf5 dataset containing tracings')
+    parser.add_argument('path_to_hdf5', type=str, help='path to hdf5 file containing tracings')
+    parser.add_argument('path_to_csv', type=str, help='path to csv file containing annotations')
+    parser.add_argument('--val_split', type=float, default=0.02, help='number between 0 and 1 determining how much of the data is to be used for validation. The remaining is used for validation. Default: 0.02')
+    parser.add_argument('--dataset_name', type=str, default='tracings', help='name of the hdf5 dataset containing tracings')
     args = parser.parse_args()
+loss = 'binary_crossentropy'
+    lr = 0.001
+    batch_size = 64
+    opt = Adam(lr)
+    callbacks = [ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, min_lr=lr / 100), EarlyStopping(patience=9, min_delta=0.00001)]
+
+    train_seq, valid_seq = ECGSequence.get_train_and_val(args.path_to_hdf5, args.dataset_name, args.path_to_csv, batch_size, args.val_split)
+
+    model = get_model(train_seq.n_classes)
+    model.compile(loss=loss, optimizer=opt)
+
+    callbacks += [TensorBoard(log_dir='./logs', write_graph=False), CSVLogger('training.log', append=False), ModelCheckpoint('./backup_model_last.hdf5'), ModelCheckpoint('./backup_model_best.hdf5', save_best_only=True)]
     # Optimization settings
     loss = 'binary_crossentropy'
     lr = 0.001
