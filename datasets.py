344db7ff255fa3744321fcf32cafcc9d0c09944f
Tejasvi-Tech <k.tejassvi2004@gmail.com>
2024-07-22 15:28:41 +0530

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -3,6 +3,42 @@ import math
 import pandas as pd
 from tensorflow.keras.utils import Sequence
 import numpy as np
+from sklearn.model_selection import train_test_split
+
+annotation_to_class = {
+    'N': 0, 'L': 0, 'R': 0, 'e': 0, 'E': 0,  # Non-Ectopic Beats
+    'A': 1, 'a': 1, 'J': 1, 'S': 1,          # Supraventricular Ectopic Beats
+    'V': 2, 'E': 2, 'F': 2,                  # Ventricular Ectopic Beats
+    'F': 3,                                  # Fusion Beats
+    'Q': 4                                   # Unknown Beats
+}
+def load_mitbih_data(data_dir):
+    record_files = [f for f in os.listdir(data_dir) if f.endswith('.dat')]
+    data = []
+    labels = []
+
+    for record_file in record_files:
+        record_path = os.path.join(data_dir, record_file[:-4])
+        record = wfdb.rdrecord(record_path)
+        annotation = wfdb.rdann(record_path, 'atr')
+        
+        signal = record.p_signal
+        raw_labels = annotation.symbol
+        label = map_annotations_to_classes(raw_labels)
+        
+        if len(label) > 0:
+            data.append(signal)
+            labels.append(label)
+    
+    data = np.array(data)
+    labels = np.array(labels)
+
+ X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
+
+def map_annotations_to_classes(raw_labels):
+    return [annotation_to_class[label] for label in raw_labels if label in annotation_to_class]
 
 
 class ECGSequence(Sequence):
